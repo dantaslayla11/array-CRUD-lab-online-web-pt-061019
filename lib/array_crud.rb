21e41d9b8472e428b7f1223b92a28317160bf45d
layla dantas <laylatdantas@gmail.com>
2019-06-23 19:06:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push "element5"
+   array = ["wow", "I", "am", "really", "learning"]
+   array << 
 end
 
 def add_element_to_start_of_array(array, element)
